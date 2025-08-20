51885d412c8c3b927becd5c5649fa32e0a9e3f19
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-08-20 00:04:32 -0800

    test

--- src/yolo.erl
+++ src/yolo.erl
@@ -6,16 +6,21 @@
 -on_load(init/0).
 
 init() ->
+    io:format("yolo:init:aaa~n"),
     LibName = "beam-yolo",
     SoName = case code:priv_dir(?MODULE) of
         {error, bad_name} ->
+            io:format("yolo:init:bbb~n"),
             case filelib:is_dir(filename:join(["..", priv])) of
                 true ->
+                    io:format("yolo:init:ccc~n"),
                     filename:join(["..", priv, LibName]);
                 _ ->
+                    io:format("yolo:init:ddd~n"),
                     filename:join([priv, LibName])
             end;
         Dir ->
+            io:format("yolo:init:eeee~n"),
             filename:join(Dir, LibName)
     end,
     io:format("Loading 'yolo' NIF from ~s~n", [SoName]),
